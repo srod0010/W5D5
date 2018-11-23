9c5e7f8207c7cf64a0df7a9aae22cd2727fdabab
srod0010 <saviofrod@gmail.com>
2018-11-23 14:03:28 -0800

    Finish add numbers callback function

--- callback_exercises.js
+++ callback_exercises.js
@@ -21,25 +21,71 @@ class Clock {
 
   _tick() {
     // 1. Increment the time by one second.
-    this.seconds = this.addSeconds() % 60;
+    // this.seconds = this.addSeconds() % 60;
+    this.addSeconds();
     // 2. Call printTime.
     this.printTime();
   }
 
   addSeconds () {
     this.seconds++;
+    if (this.seconds === 60) {
+      this.seconds = this.seconds % 60;
+      this.addMinutes();
+    }
     return this.seconds;
   }
 
   addMinutes () {
-
+    this.minutes++;
+    if (this.minutes === 60) {
+      this.minutes = this.minutes % 60;
+      this.addHours();
+    }
+    return this.minutes;
   }
 
   addHours () {
-    
+    this.hours++;
+    return this.hours;
   }
 }
 
-var clock = new Clock();
+// var clock = new Clock();
 // clock._tick();
 // clock.printTime();
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft === 0) {
+     completionCallback(sum);
+     reader.close();
+  }
+
+  if (numsLeft > 0) {
+    reader.question("Enter a number: ", function (numString) {
+      const num1 = parseInt(numString);
+      sum += num1;
+      completionCallback(sum);
+      addNumbers(sum,numsLeft - 1, completionCallback);
+    });
+  }
+
+  // reader.question("Enter #1: ", function (numString1) {
+  //   reader.question("Enter #2: ", function (numString2) {
+  //     const num1 = parseInt(numString1);
+  //     const num2 = parseInt(numString2);
+  //
+  //     callback(num1 + num2);
+  //   });
+  // });
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
