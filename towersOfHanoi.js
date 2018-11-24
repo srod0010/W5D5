3aa125ce8445685934639a5959a67425ac830a6f
srod0010 <saviofrod@gmail.com>
2018-11-23 16:53:12 -0800

    Add move and valid move methods to game

--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -4,4 +4,25 @@ class Game {
   constructor () {
     this.towers = [[],[],[3,2,1]];
   }
+
+  promptMove () {
+
+  }
+
+  move (startTower, endTower) {
+    if (this.validMove) {
+      let temp = this.towers[startTower].pop();
+      this.towers[endTower].push(temp);
+    }
+  }
+
+  validMove (startTower, endTower) {
+    if (this.towers[startTower].length === 0){
+      return false;
+    } else if (this.towers[endTower][-1] > this.towers[startTower][-1]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
