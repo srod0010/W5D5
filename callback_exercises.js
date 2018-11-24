f9b7b2428b368cf3fd5d8a3ce0cd7cd800993b1b
srod0010 <saviofrod@gmail.com>
2018-11-23 16:41:20 -0800

    Finish myBind function

--- callback_exercises.js
+++ callback_exercises.js
@@ -186,7 +186,44 @@ function absurdBubbleSort(arr, sortCompletionCallback) {
 
 // innerBubbleSortLoop([3,2,1], 0, false, (arg) => { console.log(arg)});
 
-absurdBubbleSort([3, 2, 1], function (arr) {
-  console.log("Sorted array: " + JSON.stringify(arr));
-  reader.close();
-});
+// absurdBubbleSort([3, 2, 1], function (arr) {
+//   console.log("Sorted array: " + JSON.stringify(arr));
+//   reader.close();
+// });
+
+Function.prototype.myBind = function (context) {
+  return () => this.apply(context);
+};
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
+
+// class Cat {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//
+//   meow() {
+//     console.log(`${this.name} says meow!`);
+//   }
+// }
+//
+// const curie = new Cat("Curie");
+// setTimeout(curie.meow.myBind(curie), 1000);
